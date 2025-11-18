c848dd062170dad8b3ada1b21062ae45625160bb
Vladislav <talcore@yandex.ru>
2025-11-18 20:45:12 +0500

    feat: cors add

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,11 @@
 import logging
-
-
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
+
 from backend.api.conrollers.health import router_health
 from backend.api.conrollers.company import router_company
 from backend.core.config import settings
@@ -20,6 +20,14 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(
     router_health,
