b96e09930287fb67458b68fb0daedb6c0d57f1b0
salimovaa <salimovaa@pochtabank.ru>
2025-11-25 22:33:19 +0500

    SC-02, SC-03: Добавил страницу регистрации и восстановления пароля

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "http://localhost:10004",
   withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem("accessToken");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
@@ -42,7 +42,7 @@ api.interceptors.response.use(
       if (!isRefreshing) {
         isRefreshing = true;
         try {
-          const { data } = await axios.post("http://localhost:4000/refresh-token", {}, { withCredentials: true });
+          const { data } = await axios.post("http://localhost:10004/api/v1/company/refresh-token", {}, { withCredentials: true });
           localStorage.setItem("token", data.accessToken);
           isRefreshing = false;
           processQueue(null, data.accessToken);
--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -4,7 +4,6 @@ import LoginPage from "../pages/AuthPages/LoginPage/LoginPage.jsx";
 import RegisterPage from "../pages/AuthPages/RegisterPage/RegisterPage.jsx";
 import ForgotPasswordPage from "../pages/AuthPages/ForgotPasswordPage/ForgotPasswordPage.jsx";
 import HomePage from "../pages/HomePage/HomePage.jsx";
-import CompanyRecordsPage from "../pages/CompanyRecordsPage/CompanyRecordsPage.jsx";
 import SchedulePage from "../pages/SchedulePage/SchedulePage.jsx"; 
 import ServicesPage from "../pages/ServicesPage/ServicesPage.jsx";
 import CompanySettingsPage from "../pages/CompanySettingsPage/CompanySettingsPage.jsx";
@@ -21,8 +20,6 @@ function AppRouter() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-         {/* Расписание */}
-        <Route path="/schedule" element={<SchedulePage />} />
         
         {/* Услуги */}
         <Route path="/services" element={<ServicesPage />} />
@@ -30,12 +27,13 @@ function AppRouter() {
         {/* Настройки компании */}
         <Route path="/settings" element={<CompanySettingsPage />} />
 
-        {/* Записи в компанию — только для авторизованных */}
+        {/*Только для авторизованных */}
+        {/* Расписание */}
         <Route
-          path="/records"
+          path="/schedule"
           element={
             <PrivateRoute>
-              <CompanyRecordsPage />
+              <SchedulePage />
             </PrivateRoute>
           }
         />
