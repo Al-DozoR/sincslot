656465994fb439120f7a1292ebb96ce5d6f6e7a5
Vladislav <talcore@yandex.ru>
2025-11-29 13:17:09 +0500

    fix: config log settings

--- backend/core/config.py
+++ backend/core/config.py
@@ -7,10 +7,6 @@ from pydantic_settings import (
     SettingsConfigDict,
 )
 
-LOG_DEFAULT_FORMAT = (
-    "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
-)
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
